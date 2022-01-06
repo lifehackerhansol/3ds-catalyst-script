408812c50c434b30aabc558ad29b17e3dea1120a
lifehackerhansol <lifehacker@hansol.ca>
2022-01-05 22:59:07 -0800

    Remove fredtool for USM
    ALSO:
    - Use static downloads for HBL Wrapper (hasn't updated since 2017) and Checkpoint (newer version borked)

--- catalyst-ctr.sh
+++ catalyst-ctr.sh
@@ -25,14 +25,8 @@ rm -rf FBI.zip
 echo "Downloading Anemone"
 curl -sLq https://api.github.com/repos/astronautlevel2/Anemone3DS/releases/latest | jq -r '.assets[1].browser_download_url' | wget -qi -
 
-echo "Downloading Checkpoint"
-curl -sLq https://api.github.com/repos/FlagBrew/Checkpoint/releases/latest | jq -r '.assets[1].browser_download_url' | wget -qi -
-
 echo "Downloading Universal Updater"
 curl -sLq https://api.github.com/repos/Universal-Team/Universal-Updater/releases/latest | jq -r '.assets[1].browser_download_url' | wget -qi -
-
-echo "Downloading Homebrew Launcher Wrapper" # is this just a blank cia to use as a forwarder?
-curl -sLq https://api.github.com/repos/mariohackandglitch/homebrew_launcher_dummy/releases/latest | jq -r '.assets[].browser_download_url' | wget -qi -
 
 echo "Downloading GodMode9"
 curl -sLq https://api.github.com/repos/d0k3/GodMode9/releases/latest | jq -r '.assets[].browser_download_url' | wget -qi -
@@ -52,11 +46,11 @@ wget -q "https://github.com/SciresM/boot9strap/releases/download/1.3/boot9strap-
 echo "Downloading SafeB9SInstaller"
 wget -q "https://github.com/d0k3/SafeB9SInstaller/releases/download/v0.0.7/SafeB9SInstaller-20170605-122940.zip" # Same reason as above
 
-echo "Downloading frogminer_save"
-wget -q "https://github.com/zoogie/Frogminer/releases/download/v1.0/Frogminer_save.zip" # same reason as above
+echo "Downloading Homebrew Launcher Wrapper" # is this just a blank cia to use as a forwarder?
+wget -q "https://github.com/PabloMK7/homebrew_launcher_dummy/releases/download/v1.0/Homebrew_Launcher.cia" # Same reason as above
 
-echo "Downloading b9sTool"
-wget -q "https://github.com/zoogie/b9sTool/releases/download/v6.0.1/release_6.0.1.zip" # same reason as above
+echo "Downloading Checkpoint"
+wget -q "https://github.com/FlagBrew/Checkpoint/releases/download/v3.7.4/Checkpoint.cia" # Using older release for now as newer one broke:tm:
 
 #####################
 # make some folders #
@@ -64,7 +58,7 @@ wget -q "https://github.com/zoogie/b9sTool/releases/download/v6.0.1/release_6.0.
 
 echo "Creating directories for each catalyst"
 mkdir -p {soundhax/3ds,soundhax/cias,soundhax/boot9strap,soundhax/luma/payloads} # saves me the slightest bit of time later
-mkdir -p {fredtool/3ds,fredtool/cias,fredtool/boot9strap,fredtool/luma/payloads}
+mkdir -p {usm/3ds,usm/cias,usm/luma/payloads}
 mkdir -p {pichaxx/3ds,pichaxx/cias,pichaxx/boot9strap,pichaxx/luma/payloads}
 
 echo "Creating output dir"
@@ -79,8 +73,6 @@ unzip -q Luma3DSv*.zip;rm Luma3DSv*.zip
 mkdir tmp;mv GodMode9*.zip tmp;cd tmp/;unzip -q *.zip;mv GodMode9.firm ..; mv -f gm9 ..;cd ..;rm -rf tmp
 unzip -q boot9strap-1.3.zip;rm boot9strap-1.3.zip
 mkdir tmp;mv SafeB9SInstaller*.zip tmp;cd tmp/;unzip -q *.zip;mv SafeB9SInstaller.bin ..;cd ..;rm -rf tmp
-unzip -q Frogminer_save.zip;rm Frogminer_save.zip
-mkdir tmp;mv release_6.0.1.zip tmp;cd tmp/;unzip -q *.zip;mv release_6.0.1/boot.nds ..;cd ..;rm -rf tmp
 
 ################
 # yay soundhax #
@@ -110,44 +102,43 @@ echo "Putting pichaxx related files in correct locations"
 cp *.cia pichaxx/cias
 cp *.3dsx pichaxx/3ds
 mv pichaxx/3ds/boot.3dsx pichaxx/boot.3dsx
-cp otherapp.bin pichaxx
+mv otherapp.bin pichaxx
 cp boot.firm pichaxx/boot.firm
 cp GodMode9.firm pichaxx/luma/payloads
 cp -r gm9 pichaxx/gm9
-cp boot9strap.* pichaxx/boot9strap
-cp SafeB9SInstaller.bin pichaxx
+mv boot9strap.* pichaxx/boot9strap
+mv SafeB9SInstaller.bin pichaxx
 cd pichaxx
 zip -r pichaxx.zip * 
 mv pichaxx.zip ../out
 cd ..
 rm -rf pichaxx
 
-###################
-# fwedtool is old #
-###################
+################################
+# unsafe mode is actually safe #
+################################
 
-echo "Putting fredtool related files in correct locations"
-mv *.cia fredtool/cias
-mv *.3dsx fredtool/3ds
-mv fredtool/3ds/boot.3dsx fredtool/boot.3dsx
-mv otherapp.bin fredtool
-mv boot.firm fredtool/boot.firm
-mv GodMode9.firm fredtool/luma/payloads
-mv gm9 fredtool/gm9
-mv boot9strap.* fredtool/boot9strap
-mv private fredtool/
-mv SafeB9SInstaller.bin fredtool
-mv boot.nds fredtool
-cd fredtool
-zip -r fredtool.zip * 
-mv fredtool.zip ../out
+echo "Putting USM related files in correct locations"
+mv *.cia usm/cias
+mv *.3dsx usm/3ds
+mv usm/3ds/boot.3dsx usm/boot.3dsx
+mv boot.firm usm/boot.firm
+mv GodMode9.firm usm/luma/payloads
+mv gm9 usm/gm9
+cd usm
+zip -r usm.zip * 
+mv usm.zip ../out
 cd ..
-rm -rf fredtool
+rm -rf usm
 
 
 ###########
 # Credits #
 ###########
+# 46620 for putting this on github
+#
+# and the following people that 46620 credits:
+#
 # One liner to download latest release: https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8#gistcomment-2724872
 # My friend Logan for being in voice with my while not doing his school work
 # Will Stetson for making the music I am listening to while doing this https://www.youtube.com/watch?v=8daZtdrFmBY
